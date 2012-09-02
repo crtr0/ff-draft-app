62995b4cf3be2cdf4deeb386e7a1e65a194360ac
Carter Rabasa <carter@rabasa.com>
2012-09-01 17:27:13 -0700

    fixed the code to fetch unique keys from Redis

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 var TwilioCapability = require('twilio-client-token')
   ,	redis = require("redis")
   , twiliosig = require('twiliosig')
-  , config = require('../config')
-  , client = redis.createClient(config.redis.port, config.redis.url);
+  , config = require('../config');
 
-client.auth(config.redis.password);
+
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function(req, res){  
   res.render('index');
 };
 
@@ -39,16 +38,34 @@ exports.voice = function(req, res){
 
 };
 
+var getUniqueKey = function(callback) {
+  var id = Math.floor(Math.random()*9000) + 1000;
+  var client = redis.createClient(config.redis.port, config.redis.url);
+  client.auth(config.redis.password);
+  client.get(id, function(err, res) {
+    if (err) {
+      callback(err, null);
+    }
+    else if (res) {
+      getUniqueKey(callback);
+    }
+    else {
+      client.set(id, {status: "taken"}, redis.print);
+      callback(null, id);
+    }
+  });
+};
+
 exports.create = function(req, res){
-  var id;
-  while (true) {
-  	id = Math.floor(Math.random()*9000) + 1000;
-  	if (!client.get(id)) {
-  		client.set(id, {status: "taken"}, redis.print);
-  		break;
-  	}
-  }
-  res.header('Content-type', 'application/JSON'); 
-  res.send(JSON.stringify({id: id}));
+  getUniqueKey(function(err, id) {
+    if (err) {
+      res.header('Content-type', 'application/JSON'); 
+      res.send(JSON.stringify({error: err}));
+    }
+    else {
+      res.header('Content-type', 'application/JSON'); 
+      res.send(JSON.stringify({id: id}));      
+    }
+  });
 };
 
